83e6b09997dfdd88636d7cb3ed1a6b95ff9f000c
Atem Vorobev <agvardiente@gmail.com>
2021-05-17 02:33:15 +0300

    Samurai 53-54. Class component, extends React.Component. Life cycle, componentDidMount.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,19 +1,10 @@
+import React from "react";
 import User from "./User/User";
 import "./Users.css";
 import * as axios from "axios";
 
-const Users = (props) => {
-	let getUsers = () => {
-		if (props.users.length === 0) {
-			axios
-				.get("https://social-network.samuraijs.com/api/1.0/users")
-				.then((respons) => {
-					props.setUsers(respons.data.items);
-				});
-		}
-	};
-
-	let usersElements = props.users.map((user) => {
+class Users extends React.Component {
+	/* usersElements = this.props.users.map((user) => {
 		return (
 			<User
 				key={user.id}
@@ -23,19 +14,45 @@ const Users = (props) => {
 				followed={user.followed}
 				//country={user.location.country}
 				//city={user.location.city}
-				changeFollow={props.changeFollow}
+				changeFollow={this.props.changeFollow}
 			/>
 		);
-	});
+	}); */
 
-	return (
-		<div className="users">
-			<button onClick={getUsers}>Get Users</button> <br/>
-			<div className="users__items">
-				{usersElements}
+	createUsersElements() {
+		return (
+			this.props.users.map( user => {
+				return (
+					<User
+						key={user.id}
+						id={user.id}
+						fullName={user.name}
+						status={user.status}
+						followed={user.followed}
+						changeFollow={this.props.changeFollow}
+					/>
+				)
+			})
+		)
+	}
+
+	componentDidMount() {
+		axios
+			.get("https://social-network.samuraijs.com/api/1.0/users")
+			.then((respons) => {
+				this.props.setUsers(respons.data.items);
+			});
+	}
+
+	render() {
+		return (
+			<div className="users">
+				<div className="users__items">
+					{this.createUsersElements()}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default Users;
